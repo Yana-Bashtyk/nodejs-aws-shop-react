eb3ad1d250e27c78602a36bda2cd617701fb6ac3
Yana Bashtyk <yankabashtyk16@gmail.com>
2023-07-18 22:21:33 +0400

    feat: update urls to cart

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://870nyoxs79.execute-api.eu-west-1.amazonaws.com",
-  order: "https://kia0bbgn75.execute-api.eu-west-1.amazonaws.com/api",
+  order: "https://4cmc2sx3ki.execute-api.eu-west-1.amazonaws.com/api",
   import: "https://yn3vwidmu2.execute-api.eu-west-1.amazonaws.com",
   bff: "https://870nyoxs79.execute-api.eu-west-1.amazonaws.com",
-  cart: "https://kia0bbgn75.execute-api.eu-west-1.amazonaws.com/api",
+  cart: "https://4cmc2sx3ki.execute-api.eu-west-1.amazonaws.com/api",
   tokenexchange: "https://xrepmnha3d.execute-api.eu-west-1.amazonaws.com/token-exchange",
 };
 
